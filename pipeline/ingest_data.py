169f3582441882c3ff65e8cc522be3575727449e
Devan Asokan <164494462+lemonmelonn@users.noreply.github.com>
2026-01-15 06:02:52 +0000

    finished video

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tqdm.auto import tqdm
 from sqlalchemy import create_engine
+import click
 
 
 # Specify data types (since original dataset has mixed values in some columns)
@@ -28,20 +29,29 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
+@click.command()
+@click.option('--user', default='root', help='PostgreSQL user')
+@click.option('--password', default='root', help='PostgreSQL password')
+@click.option('--host', default='localhost', help='PostgreSQL host')
+@click.option('--port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--table', default='yellow_taxi_data', help='Target table name')
+def run(user, password, host, port, db, year, month, chunksize, table):
+    pg_user = user
+    pg_pass = password
+    pg_host = host
+    pg_db = db
+    pg_port = port
 
-def run():
-    pg_user = "root"
-    pg_pass = "root"
-    pg_host = "localhost"
-    pg_db = "ny_taxi"
-    pg_port = 5432
-
-    year = 2021
-    month = 1
+    year = year
+    month = month
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/ny_taxi')
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
     # Defining an iterator that stores data in chunks of 100000
     df_iter = pd.read_csv(
@@ -49,7 +59,7 @@ def run():
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=100000,
+        chunksize=chunksize,
     )
 
 
@@ -60,7 +70,7 @@ def run():
         if first:
             # Create table schema (no data)
             df_chunk.head(0).to_sql(
-                name="yellow_taxi_data",
+                name=table,
                 con=engine,
                 if_exists="replace"
             )
@@ -69,7 +79,7 @@ def run():
 
         # Insert chunk
         df_chunk.to_sql(
-            name="yellow_taxi_data",
+            name=table,
             con=engine,
             if_exists="append"
         )
